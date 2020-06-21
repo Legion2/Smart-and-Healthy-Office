76ba75cc616d700f220c1fd3bd07d25a73f58b7e
Valentin Seifermann <valesayfa@t-online.de>
2020-06-21 18:13:31 +0200

    removed unused code

--- web-app/src/app/login/login.component.ts
+++ web-app/src/app/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Room } from '../../../generated/api/models/room';
 import { DataService } from '../shared/data.service';
-import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -13,18 +12,11 @@ export class LoginComponent implements OnInit {
 
   rooms: Observable<Array<Room>>;
 
-  constructor(private dataService: DataService,
-              private router: Router) {
+  constructor(private dataService: DataService) {
    this.rooms = dataService.getRooms();
   }
 
   ngOnInit(): void {
 
-    this.dataService.isLoggedIn.subscribe(loggedIn => {
-      if (loggedIn === false) {
-        this.router.navigate(['/login']);
-      }
-    });
   }
-
 }
